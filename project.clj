45a70f8bb94761ccd7e5c2de8e41a169392a0d77
Stan Dyck <stand@Stans-Mac-mini.homeland>
2015-04-15 13:05:47 -0700

    Bug fix: SHON.null class and root-element option

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cassidy "0.1.0"
+(defproject cassidy "0.1.1"
   :description "A SHON read and write library."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
