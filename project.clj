a8113f3a2d2b4524b144f2a0cd4b09b1792a634e
Stan Dyck <stan.dyck@gmail.com>
2015-04-16 19:24:29 -0700

    Bumping to 0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cassidy "0.1.1"
+(defproject cassidy "0.1.2"
   :description "A SHON read and write library."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
