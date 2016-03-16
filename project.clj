2b762990f48a24132fe6eeac21f3999e58fb4e9c
Stan Dyck <stan.dyck@gmail.com>
2016-03-15 21:34:33 -0700

    Added a Link record.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cassidy "0.2.3"
+(defproject cassidy "0.2.4"
   :description "A SHON read and write library."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
