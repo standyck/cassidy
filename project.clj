bd67b685c34fb85cf98b2f56a0c104806fc8d36a
Stan Dyck <stan.dyck@gmail.com>
2024-05-20 20:14:47 -0700

    Added gitlab maven repository coordinates.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cassidy "0.2.6"
+(defproject cassidy "0.2.7"
   :description "A SHON read and write library."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
@@ -6,6 +6,14 @@
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [hiccup "1.0.5"]
                  [clojure-saxon "0.9.4"]]
+  :repositories [["snapshots" {:url "https://gitlab.standyck.com/api/v4/projects/51/packages/maven"
+                               :username "Private-Token"
+                               :password :env/GITLAB_TOKEN
+                               :sign-releases false}]
+                 ["releases" {:url "https://gitlab.standyck.com/api/v4/projects/51/packages/maven"
+                              :username "Private-Token"
+                              :password :env/GITLAB_TOKEN
+                              :sign-releases false}]]
   :main ^:skip-aot cassidy.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
